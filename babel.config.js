6cbfa3d97708c5627bd35d58a03abac4ce2aafc2
Amol Chavan <Yuyutsu@users.noreply.github.com>
2026-02-27 23:34:54 +0530

    Create babel.config.js

---
+++ babel.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  presets: ['module:@react-native/babel-preset'],
+  overrides: [
+    {
+      test: /\.tsx?$/,
+      plugins: [['@babel/plugin-transform-typescript', { isTSX: true, allExtensions: true }]],
+    },
+  ],
+};
