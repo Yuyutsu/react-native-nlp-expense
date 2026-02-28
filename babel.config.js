514025c33f1a63e039465f198beea74646f43258
Amol Chavan <Yuyutsu@users.noreply.github.com>
2026-02-28 08:51:07 +0000

    issue fixes

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  presets: ['module:@react-native/babel-preset'],
-  plugins: [
-    '@babel/plugin-transform-export-namespace-from'
-  ],
+  // compile chrono-node's namespace exports to CJS so that Metro/other bundlers
+  // consuming this package never see the ES module syntax that trips them up.
+  presets: ['@babel/preset-env'],
+  plugins: ['@babel/plugin-transform-export-namespace-from'],
 };
